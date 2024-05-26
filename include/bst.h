c76e492a67fb15e849d129ccaaf9c392d11f903f
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-05-26 21:05:20 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,37 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+#include <string>
+
+template <typename T>
+struct Node {
+    T data;
+    int count; // Счетчик частоты слова
+    Node* left;
+    Node* right;
+
+    Node(const T& data) : data(data), count(1), left(nullptr), right(nullptr) {}
+};
+
+template <typename T>
+class BST {
+public:
+    Node<T>* root;
+
+    BST() : root(nullptr) {}
+
+    ~BST() {
+        clear(root);
+    }
+
+    void insert(const T& data);
+    void clear(Node<T>* node);
+
+    // Функция для частотного анализа (в alg.cpp)
+    void analyze(const std::string& filename);
+
+private:
+    Node<T>* insert(Node<T>* node, const T& data);
+    Node<T>* find(Node<T>* node, const T& data);
+};
 #endif  // INCLUDE_BST_H_

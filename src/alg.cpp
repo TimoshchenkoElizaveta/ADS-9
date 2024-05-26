4f99101c5cf78d9a6c0f626e3b70709392b3b57b
DisneyLib <160058897+TimoshchenkoElizaveta@users.noreply.github.com>
2024-05-26 21:26:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,25 +6,29 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  BST<std::string> tree;
-  std::ifstream file(filename);
-  if (!file) {
-      std::cerr << "Error opening file: " << filename << std::endl;
-      return tree;
-  }
-  std::string word;
-  char ch;
-  while (file.get(ch)) {
-      if (isalpha(ch)) {
-          word += tolower(ch); // Преобразование в нижний регистр
-      } else if (!word.empty()) {
-          tree.insert(word);
-          word.clear();
-      }
-  }
-  if (!word.empty()) { // Обработка последнего слова
-      tree.insert(word);
-  }
-  file.close();
-  return tree;
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "Error!" << std::endl;
+        exit(1);
+    }
+
+    char ch;
+    std::string word;
+    BST<std::string> tree;
+
+    while (file.get(ch)) {
+        if (isalpha(ch)) {
+            word += tolower(ch);
+        } else if (!word.empty()) {
+            tree.add(word);
+            word.clear();
+        }
+    }
+
+    if (!word.empty()) {
+        tree.add(word);
+    }
+
+    file.close();
+    return tree;
 }
